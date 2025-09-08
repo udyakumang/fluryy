69f7d811e7531977b62b82b177c7d82f759362c9
Udyak <web@fluryy.com>
2025-09-08 21:20:25 +0530

    files deleted
    files deleted

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='udyak-cache-v1';
-const OFFLINE_URLS=['/','/css/styles.css','/js/main.js','/assets/logo-udyak.svg','/assets/favicon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(OFFLINE_URLS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('/'))))});
